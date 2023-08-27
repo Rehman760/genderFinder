48893a5b22ec22a4372c907c3da54039ea9b57ef
Rehman760 <abdulrehmanq59@gmail.com>
2023-08-27 16:09:38 +0500

    "VERSION 1"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState('');
+  const [info, setInfo] = useState('');
+
+  const getInfo = async () => {
+    try {
+      setLoading(true);
+      const cleanName=name.replace(/\s/g,"")
+      const response = await fetch(`https://api.genderize.io/?name=${cleanName}`);
+      const json = await response.json();
+      setInfo(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    setName('');
+    setInfo('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gender Finder</h1>
+      <form className='form' onSubmit={submit}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={getInfo} disabled={loading}>
+          See your gender
+        </button>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="info">
+            {info.name && <p>Name: {info.name}</p>}
+            {info.gender && <p>Gender: {info.gender}</p>}
+          </div>
+        )}
+      </form>
     </div>
   );
 }
